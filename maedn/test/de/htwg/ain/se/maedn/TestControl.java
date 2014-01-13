c9918c2314996a45da987ae0a37c54493493f8cf
manuel <manuel@manuel-PC>
2014-01-13 22:00:27 +0100

    Test

---
+++ maedn/test/de/htwg/ain/se/maedn/TestControl.java
@@ -0,0 +1,14 @@
+package de.htwg.ain.se.maedn;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestControl {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
